cb1406602ee57e2486c33bd300545cba13c567f6
djgnatzy <djgnatzy@gmail.com>
2022-11-23 13:42:24 +0100

    Scr - Data en Run

--- projects/apple_disease_classification/src/run/batch_generator.py
+++ projects/apple_disease_classification/src/run/batch_generator.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
 # DEFINE IMPORTS HERE
-
+import os
+import shutil
+import random
 
 def getBatch():
 
+    origin = '../data/AQL_Data_in_Use/'
+    target = '../src/data/batch_03/'
 
+    files = os.listdir(origin)
 
+    for file_name in random.sample(files, 80):
+        shutil.copy(origin+file_name, target+file_name)
+        
+print("Files are copied successfully")
 
-# IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
+# # IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
 def main():
     pass
     
 
-
 # DO NOT IMPLEMENT ANYTHING HERE
 if __name__ == "__main__":
-    main()   
+    getBatch()   
