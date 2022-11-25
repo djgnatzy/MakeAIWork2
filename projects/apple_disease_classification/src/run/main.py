1e186584e6761aa569175fdd4ce6bf1afbbb962e
djgnatzy <djgnatzy@gmail.com>
2022-11-25 13:49:46 +0100

    Setup for Run

--- projects/apple_disease_classification/src/run/main.py
+++ projects/apple_disease_classification/src/run/main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
+import nlp_interface_sbert_v2 as sb
+
 # IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
 def main():
-    if __name__ == "__main__": 
-        print("File_run is being run directly")
-    else: 
-        print("File_run is being imported")
+    
+    sb.chatSbert()
+    
+#     if __name__ == "__main__": 
+#         print("File_run is being run directly")
+#     else: 
+#         print("File_run is being imported")
+        
+        
 
-print("File_run __name__ = %s" %__name__)
+# print("File_run __name__ = %s" %__name__)
 
 # DO NOT IMPLEMENT ANYTHING HERE
 if __name__ == "__main__":
