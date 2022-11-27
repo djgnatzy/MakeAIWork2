b3d1acc81f4c0f582034b0e32928946fb48196d2
djgnatzy <djgnatzy@gmail.com>
2022-11-27 21:13:14 +0100

    Removes redundant print statements

--- projects/apple_disease_classification/src/run/batch_classifier_v2.py
+++ projects/apple_disease_classification/src/run/batch_classifier_v2.py
@@ -33,16 +33,6 @@ def getAQLClass():
             status = 'Class 3'
         else:
             status = 'Rejected'
-         
-        # print(f'\nThe batch has been qualified as: {status}\n')
-                
-        # print (f'The total batch of {lp.sampleBatch} apples consists of:\n'
-        # f'Healthy apples:    {appleScore}    ({healthyPercentage}%)\n'
-        # f'Blotched apples:   {blotchApple}    ({blotchPercentage}%)\n'
-        # f'Rotten apples:     {rotApple}    ({rotPercentage}%)\n'
-        # f'Scabbed apples:    {scabApple}    ({scabPercentage}%)\n') 
-        
-        # print(f'The total rejected percentage is {rejectedPercentage}%, which is a total amount of', rejectedApple, 'apples.\n')
         
         return status, lp.sampleBatch, blotchApple, appleScore, rotApple, scabApple, rejectedApple, healthyPercentage, blotchPercentage, rotPercentage, rejectedPercentage, scabPercentage
 
