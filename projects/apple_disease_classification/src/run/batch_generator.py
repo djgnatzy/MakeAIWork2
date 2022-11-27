c44f9b7c397847dad024e10507e7d465070b9348
djgnatzy <djgnatzy@gmail.com>
2022-11-27 21:14:25 +0100

    Adds explanation (In progress)

--- projects/apple_disease_classification/src/run/batch_generator.py
+++ projects/apple_disease_classification/src/run/batch_generator.py
@@ -5,23 +5,28 @@ import os
 import shutil
 import random
 
+# FOLDERS IN SCR/DATA ZIJN GEVULD MET 80 RANDOM AFBEELDINGEN
+# FUNCTIE WERKT IN NOTEBOOK, HIER NOG NIET  
+
+
 def getBatch():
 
     origin = 'C:/MakeAIWork2/projects/apple_disease_classification/data/AQL_Data_in_Use/'
-    target = 'C:/MakeAIWork2/projects/apple_disease_classification/src/data/batch_01/'
+    target = 'C:/MakeAIWork2/projects/apple_disease_classification/data/batch_06/'
 
     files = os.listdir(origin)
 
     for file_name in random.sample(files, 80):
         shutil.copy(origin+file_name, target+file_name)
+    #TO DO: FIX AUTOMATIC CREATION OF FOLDERS > batch_generator_new_dir.py     
         
-print("Files are copied successfully")
-
-# # IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
+# IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
 def main():
-    pass
+    getBatch()
+    
+    print("Files are copied successfully")
     
 
 # DO NOT IMPLEMENT ANYTHING HERE
 if __name__ == "__main__":
-    getBatch()   
+    main()   
