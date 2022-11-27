fc692212cafe41f510399d983c2603297543aa74
djgnatzy <djgnatzy@gmail.com>
2022-11-27 21:15:17 +0100

    Adjusts main()

--- projects/apple_disease_classification/src/run/label_predictor.py
+++ projects/apple_disease_classification/src/run/label_predictor.py
@@ -4,7 +4,13 @@
 import numpy as np
 
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
+
+import logging, os
+
+
+logging.disable(logging.WARNING)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
 IMAGE_SIZE = 224
@@ -12,24 +18,24 @@ BATCH_SIZE = 80
 
 
 trainedModel = tf.keras.models.load_model(
-    'C:\MakeAIWork2/projects/apple_disease_classification/models/17', 
+    'C:\MakeAIWork2/projects/apple_disease_classification/models/33', 
     custom_objects=None, 
     compile=True, 
     options=None
 )
 
 aql_dataset = tf.keras.utils.image_dataset_from_directory(
-      "C:/MakeAIWork2/projects/apple_disease_classification/src/data/batches",
+      "C:/MakeAIWork2/projects/apple_disease_classification/src/data",
       shuffle=True,
       image_size=(IMAGE_SIZE, IMAGE_SIZE),
       batch_size = BATCH_SIZE,
       # labels=None
       )
 
-aql_ds = aql_dataset.skip(1)
-
 
 def getLabels():
+    
+    aql_ds = aql_dataset.skip(1)
     
     for element in aql_ds.as_numpy_iterator():
         X, y = element
@@ -42,7 +48,7 @@ def getLabels():
     statistics = dict(zip(unique2, counts2))
 
     sampleBatch = (len(prediction))
-    # print(statistics, sampleBatch)
+   
     return statistics, sampleBatch
 
 statistics, sampleBatch = getLabels()
@@ -50,10 +56,9 @@ statistics, sampleBatch = getLabels()
 
 # IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
 def main():
-    pass
+    getLabels()   
     
-
-
+    
 # DO NOT IMPLEMENT ANYTHING HERE
 if __name__ == "__main__":
-    getLabels()   
+    main()   
